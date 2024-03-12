1e6343e3b12d1f2983be0bddafa425078688ad40
agomeza2 <agomeza2@academia.ussbog.edu.co>
2024-03-12 14:38:58 -0500

    some code for simulations

---
+++ lib/dynamic-scenarios/plane_pole.py
@@ -0,0 +1,41 @@
+from vpython import * 
+plano2 = box(pos=vec(0,4.45,0),size=vec(20,1,20),axis=vec(1,-0.2,0),color=color.yellow) #planos amarillos
+plano1 = box(pos=vec(-20,5,0),size=vec(20,1,20),axis=vec(1,0.2,0),color=color.yellow)
+bloque2=box(pos=vec(0,6,0),size=vec(7.626,2.729,5),axis=vec(1,-0.2,0),color=color.red)#bloque de la derecha 
+bloque1=box(pos=vec(-20,6.7,0),size=vec(7.626,2.729,5),axis=vec(1,0.2,0),color=color.blue)#bloque de la izquierda
+polea_eje =arrow(pos=vec(-10,4.3,0),axis=vec(0,5,0),color=color.blue) #polea
+polea =sphere(pos=vec(-10,8.4,0),radius=0.7,color=color.blue)
+cuerda_izq = cylinder(pos=vec(-15.8,7,0),axis=vec(6,1.3,0), radius=0.4,color=color.red)
+cuerda_der =cylinder(pos=vec(-10,8.3,0),axis=vec(6,-1.3,0), radius=0.4,color=color.red) 
+piso=box(pos=vec(-10,0.553,0), size=vec(40,1,40),color=color.green)#piso
+
+bloque1.mass = 0.4  #masa del bloque azul
+bloque2.mass=0.3 #masa del bloque rojo 
+bloque1.vel=vec(0,0,0) #velocidades inciales
+bloque2.vel=vec(0,0,0)
+cuerda_der.vel=vec(0,0,0) 
+bloque1.a=vec(0,0,0)#aceleracion inicial
+bloque2.a=vec(0,0,0)
+uk1 = 0.1 #coeficiente cinetico del bloque azul
+uk2 = 0.2 #coeficiente cinetico del bloque rojo 
+theta_1=atan(plano1.axis.y/plano1.axis.x) #angulo izquierdo
+theta_2=atan(plano2.axis.y/plano2.axis.x) #angulo derecho 
+g=-9.77412 #aceleracion de la gravedad en el Campus Universitario  
+#aceleracion del sistema 
+a=g*((bloque2.mass*(sin(theta_2)*uk2+cos(theta_2))-bloque1.mass*(sin(theta_1)*uk1+cos(theta_1)))/(bloque1.mass-bloque2.mass))
+t=0 #tiempo incial  
+dt =0.0025 #delta de t 
+
+while t<5:
+  rate(200)
+  t+=dt
+  bloque1.pos+=bloque1.vel*dt #actualizamos posicion
+  bloque2.pos+=bloque2.vel*dt
+  cuerda_der.axis+=cuerda_der.vel*dt
+  bloque1.vel+=bloque1.a*dt #actualizamos velocidad
+  bloque2.vel+=bloque2.a*dt
+  cuerda_der.vel+=bloque2.a*dt
+  bloque1.a=vec(cos(theta_1)*a,sin(theta_1)*a,0) #aceleracion constante
+  bloque2.a=vec(cos(theta_2)*a,sin(theta_2)*a,0)
+  if not bloque1.pos.x<-14.65: #Evitar que el bloque se salga del plano 
+        break
---
+++ lib/dynamic-scenarios/table_pole.py
@@ -0,0 +1,53 @@
+from vpython import* 
+# creamos la cuerda con cilindros 
+cuerda_h = cylinder(pos=vec(-5.5,0,0),axis=vec(8.5,0,0), radius=0.1,color=color.yellow)
+cuerda_v =cylinder(pos=vec(-5.5,0,0),axis=vec(0,-5,0),radius=0.1,color=color.yellow)
+#creamos la polea con una esfera y una flecha 
+polea =sphere(pos=vec(-5.5,0,0),radius=0.2,color=color.blue)
+eje_polea=arrow(pos=vec(-5,-0.8,0),axis=vec(-0.5,0.85,0),color=color.blue)
+#creamos la superficie (mesa)
+mesa = box(pos=vec(0,-0.8,0), size=vec(10,0.1,10),color=color.green)
+#creamos los bloques (arriba-azul,abajo-rojo) 
+b_arriba = box(pos=vec(3,0,0),size=vec(1.5,1.5,1.5),color = color.blue)
+b_abajo= box(pos=vec(-5.5,-5,0),size=vec(1,1,1),color = color.red)
+    
+#masas y velocidades iniciales
+b_arriba.mass=5 #bloque azul con 5 veces mas masa
+b_abajo.mass=1
+b_arriba.vel=vec(0,0,0)
+b_abajo.vel=vec(0,0,0)
+cuerda_h.vel=vec(0,0,0)
+cuerda_v.vel=vec(0,0,0)
+
+g=-9.77412 #valor de la gravedad en Bogota en el Observatorio de la Ciudad universitaria
+uk=0.1 #coeficiente de friccion cinetico 
+t=0 #tiempo 
+dt=0.0025 #delta de t 
+#aceleracion del movimiento segun el analisis del DCL
+a=g*((b_abajo.mass-b_arriba.mass*uk)/(b_arriba.mass-b_abajo.mass)) 
+
+if a>0: #para evitar problemas con la direccion del movimiento
+  b_abajo.a=vec(0,-a,0)
+  b_arriba.a=vec(-a,0,0)
+else:
+  b_abajo.a=vec(0,a,0)
+  b_arriba.a=vec(a,0,0)
+  
+while t<5:
+    rate(300)
+    b_abajo.pos+=b_abajo.vel*dt #actualizo posicion del bloque rojo 
+    b_abajo.vel+=b_abajo.a*dt #actualizo velocidad del bloque rojo
+    b_arriba.pos+=b_arriba.vel*dt #actualizo posicion del bloque azul 
+    b_arriba.vel+=b_arriba.a*dt #actualizo velocidad del bloque azul
+    cuerda_h.axis.x+=cuerda_h.vel.x*dt #realizo el mismo procedimiento con la cuerda
+    cuerda_v.axis.y+=cuerda_v.vel.y*dt
+    if a>0:   #para evitar problemas con la direccion del movimiento
+      cuerda_h.vel.x-=a*dt
+      cuerda_v.vel.y-=a*dt
+    else:
+      cuerda_h.vel.x+=a*dt
+      cuerda_v.vel.y+=a*dt
+      
+    t+=dt
+    if not b_arriba.pos.x>-4.15: #condicion para cuando el bloque azul se estrella con la polea
+        break
