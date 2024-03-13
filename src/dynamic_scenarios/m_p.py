be110aacd86643c9db70a5b26ccf1a01c904faeb
agomeza2 <agomeza2@academia.ussbog.edu.co>
2024-03-13 13:32:30 -0500

    reading data works changed origin file, now it is only left results and simulations

--- src/dynamic_scenarios/m_p.py
+++
@@ -1,6 +0,0 @@
-from readfile import * 
-
-filename ='data_MURA.txt'
-data_array = read_file(filename)
-data_array1 = filter_numbers(data_array)
-data = float_array(data_array1)
--- src/dynamic_scenarios/plane_pole.py
+++ src/dynamic_scenarios/plane_pole.py
@@ -1,7 +1,7 @@
 from vpython import * 
 from readfile import * 
 
-filename ='data_MURA.txt'
+filename ='data_Plano inclinado polea.txt'
 data_array = read_file(filename)
 data_array1 = filter_numbers(data_array)
 data = float_array(data_array1)
